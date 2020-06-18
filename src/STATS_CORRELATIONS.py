1f804e2dca3a4535a4e32e7166bbf729ab138f33
Jin Xu <xujincdl@cn.ibm.com>
2020-06-18 02:16:51 -0700

    Update to support Python 3

--- src/STATS_CORRELATIONS.py
+++ src/STATS_CORRELATIONS.py
@@ -3,13 +3,13 @@
 # *
 # * IBM SPSS Products: Statistics Common
 # *
-# * (C) Copyright IBM Corp. 1989, 2016
+# * (C) Copyright IBM Corp. 1989, 2020
 # *
 # * US Government Users Restricted Rights - Use, duplication or disclosure
 # * restricted by GSA ADP Schedule Contract with IBM Corp. 
 # ************************************************************************/
 
-from __future__ import with_statement
+
 
 """STATS CORRELATIONS extension command"""
 
@@ -261,7 +261,7 @@ def idfNormal(p):
 def Run(args):
     """Execute the STATS CORRELATIONS extension command"""
 
-    args = args[args.keys()[0]]
+    args = args[list(args.keys())[0]]
     # debugging
     # makes debug apply only to the current thread
     #try:
@@ -301,7 +301,7 @@ def Run(args):
         def _(msg):
             return msg
     # A HELP subcommand overrides all else
-    if args.has_key("HELP"):
+    if "HELP" in args:
         #print helptext
         helper()
     else:
@@ -321,7 +321,7 @@ def helper():
     # webbrowser.open seems not to work well
     browser = webbrowser.get()
     if not browser.open_new(helpspec):
-        print("Help file not found:" + helpspec)
+        print(("Help file not found:" + helpspec))
         
 class NonProcPivotTable(object):
     """Accumulate an object that can be turned into a basic pivot table once a procedure state can be established"""
@@ -385,7 +385,7 @@ class NonProcPivotTable(object):
 def attributesFromDict(d):
     """build self attributes from a dictionary d."""
     self = d.pop('self')
-    for name, value in d.iteritems():
+    for name, value in d.items():
         setattr(self, name, value)
 
 def StartProcedure(procname, omsid):
